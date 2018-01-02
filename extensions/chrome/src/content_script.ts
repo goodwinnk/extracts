9280d0bf4c20000f4a51a7df2b0f4c3f2cf52070
Nikolay Krasko <goodwinnk@gmail.com>
2018-01-02 21:11:55 +0300

    Demo log modification

--- extensions/chrome/src/content_script.ts
+++ extensions/chrome/src/content_script.ts
@@ -1,3 +1,24 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-});
+window.onload = function () {
+    modifyLog();
+};
 
+function modifyLog() {
+    let elements = document.getElementsByClassName("commit-links-cell table-list-cell");
+    console.log("Number of found commits: " + elements.length);
+
+    let maxIndex = Math.min(elements.length, 10);
+
+    for (let i = 0; i < maxIndex; i++) {
+        let commitElement = elements.item(i);
+        commitElement.appendChild(createExtractTag("e1", "Dummy Title", "dummy"));
+    }
+}
+
+function createExtractTag(styleClass: string, title: string, extractText: string) {
+    let tagSpan = document.createElement("span");
+    tagSpan.className = "extract-tag " + styleClass;
+    tagSpan.title = title;
+    tagSpan.innerText = extractText;
+
+    return tagSpan;
+}
--- extensions/chrome/webpack.config.js
+++ extensions/chrome/webpack.config.js
@@ -5,8 +5,7 @@ module.exports = {
     entry: {
         popup: path.join(__dirname, 'src/popup.ts'),
         options: path.join(__dirname, 'src/options.ts'),
-        content_script: path.join(__dirname, 'src/content_script.ts'),
-        background: path.join(__dirname, 'src/background.ts')
+        content_script: path.join(__dirname, 'src/content_script.ts')
     },
     module: {
         loaders: [{
