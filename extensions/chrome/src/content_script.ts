7c0e4c13d14c5fab068767625ad18961f61949a7
Nikolay Krasko <goodwinnk@gmail.com>
2018-02-01 01:46:41 +0300

    Fetch extracts content on commits GitHub page

--- extensions/chrome/src/content_script.ts
+++ extensions/chrome/src/content_script.ts
@@ -1,56 +1,66 @@
 import * as ExtractsGitHub from "./github";
 import * as YamlParser from "js-yaml";
+import {githubLocation, PageKind} from "./github-location";
+import {fetchFileContent} from "./github";
 
-window.onload = function () {
-    console.log("Extracts onload");
-    console.log(YamlParser.load('greeting: hello\nname: world'));
+window.onload = async function () {
+    let gitHubLocation = githubLocation(location.href);
+    if (gitHubLocation == null) return;
 
-    ExtractsContentScript.modifyLog();
-    ExtractsGitHub.fetchCommitData("JetBrains", "kotlin", "2cdc246a274ff3fff92a7c880a905945ed6c0833");
-    ExtractsGitHub.fetchFileContent("goodwinnk", "extract", ".extracts");
+    let extractsContent = await fetchFileContent(gitHubLocation.owner, gitHubLocation.repo, ".extracts");
+    if (extractsContent == null) return;
+
+    if (gitHubLocation.kind != PageKind.commits) return;
+    let parsedDocuments = YamlParser.loadAll(extractsContent) as Array<any>;
+
+    switch (parsedDocuments.length) {
+        case 1:
+            modifyLog(parsedDocuments[0]);
+            return;
+        default:
+            return;
+    }
 };
 
-namespace ExtractsContentScript {
-    const COMMIT_CLASS_NAME = "commit";
-    const LINKS_CELL_CLASS_NAME = "commit-links-cell";
-    const COMMIT_DATA_ATTRIBUTE = "data-channel";
+const COMMIT_CLASS_NAME = "commit";
+const LINKS_CELL_CLASS_NAME = "commit-links-cell";
+const COMMIT_DATA_ATTRIBUTE = "data-channel";
 
-    // language=RegExp
-    const COMMIT_DATA_PATTERN = new RegExp("^repo:(\\w+):commit:(\\w+)$"); // repo:{number}:commit:{hash}
+// language=RegExp
+const COMMIT_DATA_PATTERN = new RegExp("^repo:(\\w+):commit:(\\w+)$"); // repo:{number}:commit:{hash}
 
-    export function modifyLog() {
-        let commitsElements = document.getElementsByClassName(COMMIT_CLASS_NAME);
-        let maxIndex = Math.min(commitsElements.length, 10);
+export function modifyLog(parsedExtracts: any) {
+    let commitsElements = document.getElementsByClassName(COMMIT_CLASS_NAME);
+    let maxIndex = Math.min(commitsElements.length, 10);
 
-        for (let i = 0; i < maxIndex; i++) {
-            let commitElement = commitsElements.item(i);
+    for (let i = 0; i < maxIndex; i++) {
+        let commitElement = commitsElements.item(i);
 
-            let linksCellElements = commitElement.getElementsByClassName(LINKS_CELL_CLASS_NAME);
-            if (linksCellElements.length != 1) continue;
+        let linksCellElements = commitElement.getElementsByClassName(LINKS_CELL_CLASS_NAME);
+        if (linksCellElements.length != 1) continue;
 
-            let dataAttribute = commitElement.getAttribute(COMMIT_DATA_ATTRIBUTE);
-            if (!dataAttribute) continue;
+        let dataAttribute = commitElement.getAttribute(COMMIT_DATA_ATTRIBUTE);
+        if (!dataAttribute) continue;
 
-            let [repoId, hash] = parseCommitData(dataAttribute);
-            console.log(`RepoId: ${repoId} Commit hash: ${hash}`);
+        let [repoId, hash] = parseCommitData(dataAttribute);
+        console.log(`RepoId: ${repoId} Commit hash: ${hash}`);
 
-            let linksCellElement = linksCellElements.item(0);
-            linksCellElement.appendChild(createExtractTag("e1", "Dummy Title", "dummy"));
-        }
+        let linksCellElement = linksCellElements.item(0);
+        linksCellElement.appendChild(createExtractTag("e1", "Dummy Title", "dummy"));
     }
+}
 
-    function parseCommitData(commitData: string): [string, string] | null {
-        let matched = COMMIT_DATA_PATTERN.exec(commitData);
-        if (matched == null) return null;
-        return [matched[1], matched[2]];
-    }
+function parseCommitData(commitData: string): [string, string] | null {
+    let matched = COMMIT_DATA_PATTERN.exec(commitData);
+    if (matched == null) return null;
+    return [matched[1], matched[2]];
+}
 
-    function createExtractTag(styleClass: string, title: string, extractText: string) {
-        let tagSpan = document.createElement("span");
-        tagSpan.className = "extract-tag " + styleClass;
-        tagSpan.title = title;
-        tagSpan.innerText = extractText;
+function createExtractTag(styleClass: string, title: string, extractText: string) {
+    let tagSpan = document.createElement("span");
+    tagSpan.className = "extract-tag " + styleClass;
+    tagSpan.title = title;
+    tagSpan.innerText = extractText;
 
-        return tagSpan;
-    }
+    return tagSpan;
 }
