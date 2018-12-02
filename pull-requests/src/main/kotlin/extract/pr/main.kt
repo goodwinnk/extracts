36279d6f78896aa481034e9846b7ab13e8dd93d9
Nikolay Krasko <goodwinnk@gmail.com>
2018-12-02 03:21:28 +0300

    -- temp

--- pull-requests/src/main/kotlin/extract/pr/main.kt
+++ pull-requests/src/main/kotlin/extract/pr/main.kt
@@ -4,29 +4,101 @@ import extract.core.*
 import org.kohsuke.github.*
 
 fun main(args: Array<String>) {
+    // Inspections -> IDE
+    // Completion? 1952, 1951 -> IDE
+    // Resolve? 1959 -> Compiler
+    // IDL2K -> Build 1950
+
     val extracts = parseFile("demo/src/main/resources/kotlin.yaml")
+    val labeledExtracts = Extracts(extracts.extracts.extracts.filter { it.labelName != null })
 
     val connect = GitHub.connect()
     val repository = connect.getRepository("JetBrains/kotlin")
 
+
+    val skip: Set<Int> = setOf(1021, 1182)
     val prs = repository.queryPullRequests()
             .state(GHIssueState.OPEN)
             .list().iterator().asSequence()
-            .take(50)
+//            .filter { it.number == 1938 }
+            .filter { it.number !in skip }
+            .filter { it.labels.isEmpty() }
+
+
+    val finalLabels = setOf(
+            "Inspections",
+            "IDEA",
+            "Completion",
+            "Backend"
+//            "J2K"
+//            "Compiler",
+    )
+
+
+    val map = mapOf<String, String>(
+            "Quickfix" to "Inspections",
+            "Intentions" to "Inspections"
+    )
+
+    val remove = mapOf<String, Set<String>>(
+            "Inspections" to setOf("IDEA")
+    )
 
     for (pr in prs) {
-        val labels = LinkedHashSet<ExtractLabel>()
+        val labels = getLabels(labeledExtracts, pr.fetchCommitInfos())
+                .map {
+                    val newName = map[it.name]
+                    if (newName != null) {
+                        it.copy(name = newName)
+                    } else {
+                        it
+                    }
+                }
 
-        val commitInfos = pr.fetchCommitInfos()
-        for (commitInfo in commitInfos) {
-            val commitLabels = assignLabels(commitInfo, extracts)
-            labels.addAll(commitLabels.filter { it.name != "YouTrack" })
+        val labelNames = labels.map { it.name }
+
+        val expectedLabels: Set<String> = labelNames.let {
+            val reducedLabelNamesSet = it.toMutableSet()
+            for ((labelName, toRemove) in remove) {
+                if (labelNames.contains(labelName)) {
+                    reducedLabelNamesSet.removeAll(toRemove)
+                }
+            }
+
+            reducedLabelNamesSet
         }
 
+        val existingLabels = pr.labels.map { it.name }
+
+        val newLabels = expectedLabels - existingLabels
+
         println("${pr.htmlUrl}: ${pr.title}")
-        println("  " + labels.joinToString { it.name })
+        println("  Existing: $existingLabels" )
+        println("  New: $newLabels")
+
+        if (!newLabels.isEmpty()) {
+
+            val assignLabels = newLabels.filter { it in finalLabels }
+            if (!assignLabels.isEmpty()) {
+
+                println("  Assign: $assignLabels")
+//                pr.setLabels(*(assignLabels + existingLabels).toTypedArray())
+            }
+        }
+
         println()
     }
+}
+
+private fun getLabels(labeledExtracts: Extracts, commitInfos: List<CommitInfo>): LinkedHashSet<ExtractLabel> {
+    val labels = LinkedHashSet<ExtractLabel>()
+
+    for (commitInfo in commitInfos) {
+        val commitLabels = assignLabels(commitInfo, labeledExtracts)
+        labels.addAll(commitLabels)
+    }
+
+    return labels
 }
 
 private fun GHPullRequest.fetchCommitInfos(): List<CommitInfo> {
@@ -45,10 +117,17 @@ private fun GHPullRequest.fetchCommitInfos(): List<CommitInfo> {
 
     result.add(titleInfo)
 
-    listCommits().asList().mapTo(result) { commitDetails ->
-        val commit = repository.getCommit(commitDetails.sha)
-        commit.toCommitInfo()
-    }
+    listCommits()
+            .asSequence()
+            .map { commitDetails ->
+                repository.getCommit(commitDetails.sha)
+            }
+            .filter { commit ->
+                commit.parents.size <= 1 // Filter out merge commits
+            }
+            .mapTo(result) { commit ->
+                commit.toCommitInfo()
+            }
 
     return result
 }
